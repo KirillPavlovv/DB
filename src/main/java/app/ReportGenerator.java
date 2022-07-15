d45f72b5ec3e7982bf9b46a93dd5eecec9a5acf1
Kirillp1988 <kirillp1988@gmail.com>
2022-07-15 22:38:38 +0300

    separate method for totalBalance

--- src/main/java/app/ReportGenerator.java
+++ src/main/java/app/ReportGenerator.java
@@ -5,6 +5,7 @@ import database.Payment;
 import database.Report;
 import database.ReportLine;
 import lombok.RequiredArgsConstructor;
+import net.minidev.json.JSONObject;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -20,7 +21,7 @@ public class ReportGenerator {
 
     public static void main(String[] args) {
         ReportGenerator reportGenerator = new ReportGenerator();
-        reportGenerator.generateReport(UUID.fromString("0b0adb2b-bffc-4bbc-a223-e27e57f01421"));
+        reportGenerator.generateReport(UUID.fromString("dd115157-a995-4b0a-82e1-e3f56a09185e"));
     }
 
     private void generateReport(UUID customerId) {
@@ -30,38 +31,46 @@ public class ReportGenerator {
         List<Payment> payments = getPaymentsByCustomerId(customerId);
         List<Invoice> invoices = getInvoicesByCustomerID(customerId);
         List<ReportLine> reportLines = new ArrayList<>();
-        BigDecimal totalBalance = BigDecimal.ZERO;
+        report.setBalance(getTotalBalance(payments, invoices));
 
         for (Invoice invoice : invoices) {
             BigDecimal balance = BigDecimal.ZERO;
             ReportLine reportLine = new ReportLine();
             reportLine.setInvoice(invoice);
             balance = balance.subtract(invoice.getAmount());
-            totalBalance = totalBalance.subtract(invoice.getAmount());
             Map<UUID, BigDecimal> map = new HashMap<>();
             for (Payment payment : payments) {
                 if(payment.getAmount().compareTo(BigDecimal.ZERO)>0) {
                     balance = balance.add(payment.getAmount());
                     if (balance.compareTo(BigDecimal.ZERO) > 0) {
                         BigDecimal remainder = payment.getAmount().subtract(balance);
-                        totalBalance = totalBalance.add(remainder);
                         map.put(payment.getId(), remainder);
                         payment.setAmount(balance);
                         break;
                     } else {
-                        totalBalance = totalBalance.add(payment.getAmount());
                         map.put(payment.getId(), payment.getAmount());
                         payment.setAmount(BigDecimal.ZERO);
                     }
                 }
             }
-            report.setBalance(totalBalance);
             reportLine.setPayments(map);
             reportLines.add(reportLine);
         }
         System.out.println(reportLines);
         System.out.println(report.getBalance());
 
+    }
+
+    private BigDecimal getTotalBalance(List<Payment> payments, List<Invoice> invoices) {
+        BigDecimal totalBalance = BigDecimal.ZERO;
+
+        for (Invoice invoice : invoices) {
+            totalBalance = totalBalance.subtract(invoice.getAmount());
+        }
+        for (Payment payment : payments) {
+            totalBalance = totalBalance.add(payment.getAmount());
+        }
+        return totalBalance;
     }
 
     private void setCustomerName(UUID customerId, Report report) {
